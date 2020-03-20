de8b2034d9a6f0fc1931fc55c08780251aa06767
MateuszPluta <m.pluta90@gmail.com>
2020-03-20 21:42:22 +0100

    CMS Page Controller

--- ShopUZ/Areas/Admin/Controllers/PagesController.cs
+++ ShopUZ/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using ShopUZ.Models.Data;
+using ShopUZ.Models.ViewModels.Pages;
 
 namespace ShopUZ.Areas.Admin.Controllers
 {
@@ -7,7 +11,19 @@ namespace ShopUZ.Areas.Admin.Controllers
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+            //Deklaracja listy PageViewModel
+            List<PageVM> pagesList;
+            
+            using (Db db = new Db())
+            {
+                //Inicializacja listy
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Zwracamy strony do widoku
+
+
+            return View(pagesList);
         }
     }
 }
