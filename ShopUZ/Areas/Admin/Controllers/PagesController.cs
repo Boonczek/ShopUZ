ed7defd61904f5634ddee0f6902698974cb941c4
MateuszPluta <m.pluta90@gmail.com>
2020-03-21 16:44:09 +0100

    Dodawanie nowej strony c.d

--- ShopUZ/Areas/Admin/Controllers/PagesController.cs
+++ ShopUZ/Areas/Admin/Controllers/PagesController.cs
@@ -27,10 +27,59 @@ namespace ShopUZ.Areas.Admin.Controllers
         }
 
         // GET: Admin/Pages/AddPage
+        [HttpGet]
         public ActionResult AddPage()
         {
 
             return View();
+        }
+
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //Sprawdzanie Model state stanu formularza
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            } 
+
+            using (Db db = new Db())
+            {
+                string slug;
+                //inicializacja PageDTO
+                PageDTO dto = new PageDTO();
+               
+                //Gdy nie mamy adresu strony to przypisujemy tytu
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();    
+                }
+
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                //Zapobieagamy dodania takiej samej nazwy strony to przypisujemy tytuł 
+                if(db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "Ten tytuł lub adres strony już istnieje.");
+                        return View(model);
+                }
+                dto.Title = model.Title;
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 1000;
+
+                // Zapis dto
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Dodałeś nową stronę!";
+            return RedirectToAction("AddPage");
         }
     }
 }
