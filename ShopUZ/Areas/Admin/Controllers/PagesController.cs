13676bd3c2e13aa29b5b91112e6cd4f52978db8c
MateuszPluta <m.pluta90@gmail.com>
2020-03-21 21:06:54 +0100

    Edycja strony czesc 2

--- ShopUZ/Areas/Admin/Controllers/PagesController.cs
+++ ShopUZ/Areas/Admin/Controllers/PagesController.cs
@@ -102,6 +102,52 @@ namespace ShopUZ.Areas.Admin.Controllers
 
             return View(model);
         }
+
+        // POST: Admin/Pages/EditPage
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+                //pobranie Id strony
+                int id = model.Id;
+
+                //inicializacja slug
+                string slug = "home";
+
+                //pobranie strony do edycji
+                PageDTO dto = db.Pages.Find(id);
+
+                dto.Title = model.Title;
+
+                if(model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    }
+
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower(); 
+                    }
+                }
+
+                if(db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title) || 
+                    db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug)
+                {
+
+                }
+
+            }
+            return View();
+        }
+
     }
 
 }
