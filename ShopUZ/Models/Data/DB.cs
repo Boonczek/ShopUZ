487fbb300b25beedda3113858af37b9ec22fd206
MateuszPluta <m.pluta90@gmail.com>
2020-04-29 21:17:34 +0200

    tblProducts, ProductDTO, ProductVM - dodawanie produktów

--- ShopUZ/Models/Data/DB.cs
+++ ShopUZ/Models/Data/DB.cs
@@ -8,5 +8,7 @@ namespace ShopUZ.Models.Data
         public DbSet<PageDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
         public DbSet<CategoryDTO> Categories { get; set; }
+
+        public DbSet<ProductDTO> Products { get; set; }
     }
 }
