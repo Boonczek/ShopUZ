adeea572126227e33067c752afa8e8bfe2bf52d5
MateuszPluta <m.pluta90@gmail.com>
2020-05-01 11:49:41 +0200

    ShopController- dodawanie zdjec produktów i ustawianie struktury katalogów.

--- ShopUZ/Areas/Admin/Controllers/ShopController.cs
+++ ShopUZ/Areas/Admin/Controllers/ShopController.cs
@@ -1,6 +1,7 @@
 ﻿using ShopUZ.Models.Data;
 using ShopUZ.Models.ViewModels.Shop;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -200,6 +201,47 @@ namespace ShopUZ.Areas.Admin.Controllers
 
             #region Upload Image
             #endregion
+            //Utworzenie potrzebne struktury katalogów
+            var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+
+            var pathString1 = Path.Combine(originalDirectory.ToString(), "Products");
+            var pathString2 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString());
+            var pathString3 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Thumbs");
+            var pathString4 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery");
+            var pathString5 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery\\Thumbs" );
+
+            if (!Directory.Exists(pathString1))
+                Directory.CreateDirectory(pathString1);
+            if (!Directory.Exists(pathString2))
+                Directory.CreateDirectory(pathString2);
+            if (!Directory.Exists(pathString3))
+                Directory.CreateDirectory(pathString3);
+            if (!Directory.Exists(pathString4))
+                Directory.CreateDirectory(pathString4);
+            if (!Directory.Exists(pathString5))
+                Directory.CreateDirectory(pathString5);
+
+            if(file != null && file.ContentLength > 0)
+            {
+                //sprawdzenie rozszerzenia pliku(czy jest to obrazek)
+                string ext = file.ContentType.ToLower();
+                if (ext != "image/jpg" &&
+                    ext != "image/jpeg" &&
+                    ext != "image/pjpeg" &&
+                    ext != "image/gif" &&
+                    ext != "image/x-png" &&
+                    ext != "image/png")
+                {
+
+                    using (Db db = new Db())
+                    {
+                        model.Categories = new SelectList(db.Categories.ToList(), "Id", "Name");
+                        ModelState.AddModelError("", "Obraz nie został przesłąny- nieprawidłowe rozszerzenie obrazu!");
+                        return View(model);
+                    }   
+                }
+            }
+
 
             return View();
         }
