4abe853f949b654d03ec9a1087f7af71f2c2e22f
Boonczek <Boonczek@users.noreply.github.com>
2020-05-22 19:13:17 +0200

    Dodanie profilu użytkownika

--- ShopUZ/Controllers/AccountController.cs
+++ ShopUZ/Controllers/AccountController.cs
@@ -168,5 +168,28 @@ namespace ShopUZ.Controllers
 
             return PartialView(model);
         }
+
+        // GET: /account/user-profile
+        [HttpGet]
+        [ActionName("user-profile")]
+        [Authorize]
+        public ActionResult UserProfile()
+        {
+            // pobieramy nazwe uzytkownika
+            string username = User.Identity.Name;
+
+            //deklarujemy model
+            UserProfileVM model;
+
+            using (Db db = new Db())
+            {
+                // pobieramy uzytkownika
+                UserDTO dto = db.Users.FirstOrDefault(x => x.UserName == username);
+
+                model = new UserProfileVM(dto);
+            }
+
+            return View("UserProfile", model);
+        }
     }
 }
